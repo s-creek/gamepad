5468a049ae8a7670ffe23df56133188fafc544af
wu <wu@hostname.(none)>
2014-12-29 12:15:01 +0900

    first commit

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export COMP_NAME="GamepadRTC"
+export PATH=/usr/pkg/bin:$PATH
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+#export LD_LIBRARY_PATH=/usr/pkg/lib:/usr/local/lib:/usr/lib/OpenRTM-aist
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/openrtm-1.1
