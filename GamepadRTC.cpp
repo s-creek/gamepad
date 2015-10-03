556278b8498f2cdc64afcc3c910be1506c204ed3
s-creek <ogawa@oga-pc04.(none)>
2015-10-03 22:06:24 +0900

    fork

--- GamepadRTC.cpp
+++ GamepadRTC.cpp
@@ -6,7 +6,7 @@
 
 // Module specification
 static const char* gamepad_spec[] =
-{
+  {
     "implementation_id", "GamepadRTC",
     "type_name",         "GamepadRTC",
     "description",       "Access a joystick control device.",
@@ -22,39 +22,39 @@ static const char* gamepad_spec[] =
     "conf.default.device", "/dev/input/js0",
     "conf.default.debugLevel", "0",
     ""
-};
+  };
 
 
 GamepadRTC::GamepadRTC(RTC::Manager* manager)
-    : RTC::DataFlowComponentBase(manager),
-      m_axesOut("axes", m_axes),
-      m_buttonsOut("buttons", m_buttons),
-      m_debugLevel(0)
+  : RTC::DataFlowComponentBase(manager),
+    m_axesOut("axes", m_axes),
+    m_buttonsOut("buttons", m_buttons),
+    m_debugLevel(0)
 {
-    m_joystick = 0;
+  m_joystick = 0;
 }
 
 
 GamepadRTC::~GamepadRTC()
 {
-    if(m_joystick){
-        delete m_joystick;
-    }
+  if(m_joystick){
+    delete m_joystick;
+  }
 }
 
 
 RTC::ReturnCode_t GamepadRTC::onInitialize()
 {
-    // Bind variables and configuration variable
-    bindParameter("device", m_device, "/dev/input/js0");
-    bindParameter("debugLevel", m_debugLevel, "0");
+  // Bind variables and configuration variable
+  bindParameter("device", m_device, "/dev/input/js0");
+  bindParameter("debugLevel", m_debugLevel, "0");
 
-    // Set OutPort buffer
-    addOutPort("axes", m_axesOut);
-    addOutPort("buttons", m_buttonsOut);
+  // Set OutPort buffer
+  addOutPort("axes", m_axesOut);
+  addOutPort("buttons", m_buttonsOut);
   
 
-    return RTC::RTC_OK;
+  return RTC::RTC_OK;
 }
 
 
@@ -63,95 +63,95 @@ RTC::ReturnCode_t GamepadRTC::onActivated(RTC::UniqueId ec_id)
   //for debug
   //m_debugLevel=1;
 
-    if(m_debugLevel > 0){
-        std::cout << "GamepadRTC::onActivated(" << ec_id << ")" << std::endl;
-    }
+  if(m_debugLevel > 0){
+    std::cout << "GamepadRTC::onActivated(" << ec_id << ")" << std::endl;
+  }
 
-    if(!m_joystick){
-        m_joystick = new cnoid::Joystick(m_device.c_str());
+  if(!m_joystick){
+    m_joystick = new cnoid::Joystick(m_device.c_str());
+  }
+  if(!m_joystick->isReady()){
+    std::cerr << "Gamepad device(" << m_device << ") is not opened" << std::endl;
+    return RTC::RTC_ERROR;  
+  } else {
+    int n = m_joystick->numAxes();
+    m_axes.data.length(n);
+    for(int i=0; i < n; ++i){
+      m_axes.data[i] = m_joystick->getPosition(i);
     }
-    if(!m_joystick->isReady()){
-        std::cerr << "Gamepad device(" << m_device << ") is not opened" << std::endl;
-        return RTC::RTC_ERROR;  
-    } else {
-        int n = m_joystick->numAxes();
-        m_axes.data.length(n);
-        for(int i=0; i < n; ++i){
-            m_axes.data[i] = m_joystick->getPosition(i);
-        }
-        int m = m_joystick->numButtons();
-        m_buttons.data.length(m);
+    int m = m_joystick->numButtons();
+    m_buttons.data.length(m);
 
-	//std::cout<<"num "<<n<<" "<<m<<std::endl;
+    //std::cout<<"num "<<n<<" "<<m<<std::endl;
 
-        for(int i=0; i < m; ++i){
-            m_buttons.data[i] = m_joystick->getButtonState(i);
-        }
-        return RTC::RTC_OK;
+    for(int i=0; i < m; ++i){
+      m_buttons.data[i] = m_joystick->getButtonState(i);
     }
+    return RTC::RTC_OK;
+  }
 }
 
 
 RTC::ReturnCode_t GamepadRTC::onDeactivated(RTC::UniqueId ec_id)
 {
-    if(m_debugLevel > 0){
-        std::cout << "GamepadRTC::onDeactivated(" << ec_id << ")" << std::endl;
-    }
+  if(m_debugLevel > 0){
+    std::cout << "GamepadRTC::onDeactivated(" << ec_id << ")" << std::endl;
+  }
 
-    if(m_joystick){
-        delete m_joystick;
-        m_joystick = 0;
-    }
+  if(m_joystick){
+    delete m_joystick;
+    m_joystick = 0;
+  }
     
-    return RTC::RTC_OK;
+  return RTC::RTC_OK;
 }
 
 
 
 RTC::ReturnCode_t GamepadRTC::onExecute(RTC::UniqueId ec_id)
 {
-    m_joystick->readCurrentState();
+  m_joystick->readCurrentState();
     
+  if(m_debugLevel > 0){
+    std::cout << "axes:";
+  }
+  const int n = m_joystick->numAxes();
+  for(int i=0; i < n; ++i){
+    m_axes.data[i] = m_joystick->getPosition(i);
     if(m_debugLevel > 0){
-        std::cout << "axes:";
+      std::cout << " " << m_axes.data[i];
     }
-    const int n = m_joystick->numAxes();
-    for(int i=0; i < n; ++i){
-        m_axes.data[i] = m_joystick->getPosition(i);
-        if(m_debugLevel > 0){
-            std::cout << m_axes.data[i];
-        }
-    }
+  }
+  if(m_debugLevel > 0){
+    std::cout << "\n" << "buttons:";
+  }
+  const int m = m_joystick->numButtons();
+  //std::cout<<"num "<<n<<" "<<m<<std::endl;
+
+  for(int i=0; i < m; ++i){
+    m_buttons.data[i] = m_joystick->getButtonState(i);
     if(m_debugLevel > 0){
-        std::cout << ", buttons:";
+      std::cout << " " << m_buttons.data[i];
     }
-    const int m = m_joystick->numButtons();
-    //std::cout<<"num "<<n<<" "<<m<<std::endl;
+  }
+  if(m_debugLevel > 0){
+    std::cout << std::endl;
+  }
 
-    for(int i=0; i < m; ++i){
-        m_buttons.data[i] = m_joystick->getButtonState(i);
-        if(m_debugLevel > 0){
-            std::cout << m_buttons.data[i];
-        }
-    }
-    if(m_debugLevel > 0){
-        std::cout << std::endl;
-    }
+  m_axesOut.write();
+  m_buttonsOut.write();
 
-    m_axesOut.write();
-    m_buttonsOut.write();
-
-    return RTC::RTC_OK;
+  return RTC::RTC_OK;
 }
 
 
 extern "C"
 {
-    void GamepadRTCInit(RTC::Manager* manager)
-    {
-        coil::Properties profile(gamepad_spec);
-        manager->registerFactory(profile,
-                                 RTC::Create<GamepadRTC>,
-                                 RTC::Delete<GamepadRTC>);
-    }
+  void GamepadRTCInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(gamepad_spec);
+    manager->registerFactory(profile,
+			     RTC::Create<GamepadRTC>,
+			     RTC::Delete<GamepadRTC>);
+  }
 };
